54423f15e9b74799a702104746a505056a2419f9
Jairo Domingos Buto <jairobuto03@gmail.com>
2025-06-03 14:55:01 +0100

    Criação da tabela contaContabilistica na migração e resolucao de um conflito com o vite.

---
+++ database/migrations/2025_06_02_195724_create__conta_contabilistica_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contaContabilistica', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo',20);
+            $table->string('descricao',255);
+            $table->enum('tipo',['A','T','S']);
+            $table->int('pai_id');//INT (FK ->ContaContabilistica.id)
+            $table->string('classe',10);
+            $table->string('moeda',3);//"AKZ", "USD"
+            $table->boolean('ativa');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contaContabilistica');
+    }
+};
